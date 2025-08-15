0cf9f4753671841389ee1d5a1744a07147dc284e
zhongyi <yi.zhong@xgo.one>
2025-08-15 17:35:33 +0800

    feat: init

--- components/clients/Test.tsx
+++ components/clients/Test.tsx
@@ -1,34 +1,38 @@
 'use client'
 
-import { useTranslations } from "next-intl";
+import { useTranslations } from 'next-intl'
 
 export default function Test() {
-    const t = useTranslations('HomePage');
-    const items = [
-        {
-            title: t('yearsOfService.title'),
-            value: t('yearsOfService.value')
-        },
-        {
-            title: t('happyClients.title'),
-            value: t('happyClients.value')
-        },
-        {
-            title: t('partners.title'),
-            value: t('partners.value')
-        }
-    ];
-    console.info("🚀 ~ file:client -----",)
-    return (
-        <div>{t('title')}
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>
-                        <h2>{item.title}</h2>
-                        <p>{item.value}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+	const t = useTranslations('HomePage')
+	const items = [
+		{
+			title: t('yearsOfService.title'),
+			value: t('yearsOfService.value')
+		},
+		{
+			title: t('happyClients.title'),
+			value: t('happyClients.value')
+		},
+		{
+			title: t('partners.title'),
+			value: t('partners.value')
+		}
+	]
+	console.info('🚀 ~ file:client -----')
+	return (
+		<div>
+			<h1>
+				环境变量: {process.env.NODE_ENV} | {process.env.NEXT_PUBLIC_MODE_ENV}
+			</h1>
+			{t('title')}
+			<ul>
+				{items.map((item, index) => (
+					<li key={index}>
+						<h2>{item.title}</h2>
+						<p>{item.value}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true
+	},
+	typescript: {
+		ignoreBuildErrors: true
+	}
+}
+const withNextIntl = createNextIntlPlugin()
+const nextApplication = withNextIntl(nextConfig)
 
-const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+if (process.env.NODE_ENV === 'development') {
+	nextApplication.rewrites = async () => {
+		console.log(process.env.NODE_ENV, 'process.env.NODE_ENV')
+		// 去中间件排除对应路径
+		return [
+			// {
+			// 	// 自身服务端接口
+			// 	source: '/nextapi/:path*',
+			// 	destination: 'http://127.0.0.1:7815/nextapi/:path*'
+			// },
+		]
+	}
+}
+export default nextApplication
